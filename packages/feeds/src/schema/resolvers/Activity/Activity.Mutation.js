4af1ba92faf084c81ce95adb7d602eb5c85b8fb9
Luke Smetham <luke@smetham.dev>
2020-10-31 10:53:34 +0100

    addActivity mutation

--- packages/feeds/src/schema/resolvers/Activity/Activity.Mutation.js
+++ packages/feeds/src/schema/resolvers/Activity/Activity.Mutation.js
@@ -1,5 +1,13 @@
 export const Mutation = {
     addActivity: {
-        resolve: () => {},
+        resolve: (_, { feed, data }, { stream: { feeds } }) => {
+            const activity = data;
+
+            if (activity.to?.length) {
+                activity.to = activity.to.map(feedSelector => feedSelector.together);
+            }
+
+            return feeds.feed(...feed).addActivity(activity);
+        },
     },
 };
