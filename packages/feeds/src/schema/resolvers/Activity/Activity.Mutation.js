94aa1998f52089b07efba49ff47ec188741f7ff0
Luke Smetham <luke@smetham.dev>
2020-10-31 11:41:29 +0100

    cleanup

--- packages/feeds/src/schema/resolvers/Activity/Activity.Mutation.js
+++ packages/feeds/src/schema/resolvers/Activity/Activity.Mutation.js
@@ -2,7 +2,7 @@ export const Mutation = {
     addActivity: {
         resolve: (_, { feed, data }, { stream: { feeds } }) => {
             const activity = data;
-            console.log(feed);
+
             if (activity.to?.length) {
                 activity.to = activity.to.map(StreamSelector => StreamSelector.together);
             }
