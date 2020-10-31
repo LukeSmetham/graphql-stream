5bcf9d23455babaad4c6d0669d735507b23ad43a
Luke Smetham <luke@smetham.dev>
2020-10-31 10:53:50 +0100

    Update Feed.Query.js

--- packages/feeds/src/schema/resolvers/Feed/Feed.Query.js
+++ packages/feeds/src/schema/resolvers/Feed/Feed.Query.js
@@ -1,11 +1,13 @@
 export const Query = {
     feed: {
-        resolve: async (_, { feed, slug, id, ...options }, { stream }) => {
+        resolve: async (_, { slug, id, ...options }, { stream }) => {
             if (!slug || !id) {
                 throw new Error('Must provide a feed selector, or a slug and id');
             }
 
-            const { results } = await stream.feeds.feed(slug, id).get(options);
+            const feed = stream.feeds.feed(slug, id);
+
+            const { results } = await feed.get(options);
 
             return results;
         },
