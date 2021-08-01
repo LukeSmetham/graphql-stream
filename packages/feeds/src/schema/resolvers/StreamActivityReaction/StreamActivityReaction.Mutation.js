e5dfaf3924c57dbb5152db294e62495602c985bd
Luke Smetham <luke@arkitek.co.uk>
2021-08-01 13:09:42 +0100

    added updateReaction and deleteReaction resolvers

--- packages/feeds/src/schema/resolvers/StreamActivityReaction/StreamActivityReaction.Mutation.js
+++ packages/feeds/src/schema/resolvers/StreamActivityReaction/StreamActivityReaction.Mutation.js
@@ -5,4 +5,21 @@ export const Mutation = {
 			return reaction;
         },
     },
+    updateReaction: {
+        resolve: async (source, { id, data }, { stream }) => {
+			const reaction = await stream.feeds.reactions.update(id, data)
+			return reaction;
+        },
+    },
+    deleteReaction: {
+        resolve: async (source, { id }, { stream }) => {
+			try {
+				await stream.feeds.reactions.delete(id);
+				return true;
+			} catch (error) {
+				console.error(error.message);
+				return false;
+			}
+        },
+    },
 };
