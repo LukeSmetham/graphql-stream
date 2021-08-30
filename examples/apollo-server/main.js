4f0248f17869723ea8c32e2ef6a51f2ce73fc79f
Luke Smetham <luke@arkitek.co.uk>
2021-08-30 11:01:22 +0100

    Update main,js to use new composer-based version

--- examples/apollo-server/main.js
+++ examples/apollo-server/main.js
@@ -1,14 +1,36 @@
 import 'dotenv/config';
 import { ApolloServer } from 'apollo-server';
-import { schema } from '@stream-io/graphql-feeds';
+import { createActivityFeed } from '@graphql-stream/feeds';
+import { schemaComposer } from 'graphql-compose';
 
 import context from './context';
 
 const { PORT = 8080 } = process.env;
 
+// Adds the generated schema to an existing schemaComposer
+// createActivityFeedsSchema(schemaComposer);
+
+// Creates a standalone schema (useful if the implementing server doesn't use gql-compose.)
+// const schema = createActivityFeedsSchema();
+
+// Creates an activity feed, allows customization opts across multiple feeds.
+schemaComposer.Query.addFields({
+    userFeed: createActivityFeed({
+        feedGroup: 'user',
+        type: 'flat',
+    }),
+    timeline: createActivityFeed({
+        feedGroup: 'timeline',
+        type: 'aggregated',
+        activityFields: {
+            src: 'String!',
+        },
+    }),
+});
+
 const server = new ApolloServer({
     context,
-    schema,
+    schema: schemaComposer.buildSchema(),
 });
 
 server.listen(PORT);
