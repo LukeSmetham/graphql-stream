c9c40827d83ac4c3786a29b8ac5de82dfe6bf572
Luke Smetham <luke@arkitek.co.uk>
2021-08-30 11:06:08 +0100

    WIP, returns a full feeds schema with less customization options
    Only useful when using out of the box, and not attaching to other existing types - need to add customization opts such as a map of feed groups etc.

--- packages/feeds-compose/src/index.js
+++ packages/feeds-compose/src/index.js
@@ -1,2 +1,18 @@
-export * from './resolvers';
+import { SchemaComposer } from 'graphql-compose';
+import { createActivityFeed } from './types/Feed';
+
+export const createActivityFeedsSchema = composer => {
+    const schemaComposer = composer || new SchemaComposer();
+
+    schemaComposer.Query.addFields({
+        feed: createActivityFeed({
+            schemaComposer,
+        }),
+    });
+
+    const schema = schemaComposer.buildSchema();
+
+    return schema;
+};
+
 export * from './types';
