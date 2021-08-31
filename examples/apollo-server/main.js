d2072fe13422569284b97d19f908ee39da2a6a09
Luke Smetham <luke@arkitek.co.uk>
2021-08-31 09:16:59 +0100

    Update main.js

--- examples/apollo-server/main.js
+++ examples/apollo-server/main.js
@@ -5,6 +5,8 @@ import { schemaComposer } from 'graphql-compose';
 
 const { STREAM_KEY, STREAM_SECRET, STREAM_ID, PORT = 8080 } = process.env;
 
+// TODO: Add example for optional context that we can auth the stream user from (i.e. emulate client side auth for protection against certain actions from the client)
+
 const credentials = {
     api_key: STREAM_KEY,
     api_secret: STREAM_SECRET,
