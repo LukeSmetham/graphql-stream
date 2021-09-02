52e56389cae5c78dbc1d0f4fa2f1950a0ed366f3
Luke Smetham <luke@arkitek.co.uk>
2021-09-02 11:55:14 +0100

    Update createUsers to add the resolvers to the thype directly rather than returning an object of "resolver creator fns"

--- packages/feeds-compose/src/composeActivityFeed/createUsers.js
+++ packages/feeds-compose/src/composeActivityFeed/createUsers.js
@@ -1,36 +1,13 @@
 import { createUserTC } from 'types/User';
 
-import { addUser, getUser, getOrCreateUser, removeUser, updateUser } from 'types/User/resolvers';
+import * as resolvers from 'types/User/resolvers';
 
 export const createUsers = options => {
     const UserTC = createUserTC(options);
 
-    return {
-        UserTC,
-        query: {
-            getUser: () => getUser(UserTC, options),
-            getOrCreateUser: () => getOrCreateUser(UserTC, options),
-        },
-        mutation: {
-            addUser: () => addUser(UserTC, options),
-            updateUser: () => updateUser(UserTC, options),
-            removeUser: () => removeUser(UserTC, options),
-        },
-        subscription: {},
-    };
+    Object.keys(resolvers).forEach(k => {
+        UserTC.addResolver(resolvers[k](UserTC, options));
+    });
+
+    return UserTC;
 };
-
-// NOTE: Adding Resolvers as below, we can make it easy for devs to pull types out of the store from anywhere in there server code.
-// Branch off and test this out with the Feed and Activity types too.
-//
-// import * as resolvers from 'types/User/resolvers';
-
-// export const createUsers = options => {
-//     const UserTC = createUserTC(options);
-
-//     Object.keys(resolvers).forEach(k => {
-//         UserTC.addResolver(resolvers[k](UserTC, options));
-//     });
-
-//     return UserTC;
-// };
