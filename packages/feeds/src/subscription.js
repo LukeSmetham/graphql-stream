8d5d9b7986d11e1beddeef88a98357be3404b34e
Luke Smetham <luke@arkitek.co.uk>
2021-09-03 21:48:41 +0100

    added Faye client to FeedSubscription class

--- packages/feeds/src/subscription.js
+++ packages/feeds/src/subscription.js
@@ -1,45 +1,98 @@
 import { PubSubEngine } from 'graphql-subscriptions';
+import * as Faye from 'faye';
+import jwt from 'jsonwebtoken';
 
 export class FeedSubscription extends PubSubEngine {
+    fayeClient;
+
     currentSubscriptionId = 0;
 
     subscriptions = {};
 
     subRefMap = {};
 
-    constructor(client) {
+    constructor(credentials) {
         super();
 
-        if (!client) {
-            throw new Error('Missing client instance when initializing FeedsSubscription.');
+        if (!credentials) {
+            throw new Error('Missing Stream Feeds credentials when initializing FeedsSubscription.');
         }
 
-        if (!client.appId) {
-            throw new Error('Missing Stream App ID - Make sure you pass it as the third argument to the Feeds Client.');
+        if (!credentials.app_id) {
+            throw new Error('Missing Stream App ID - Make sure you pass it with your credentials in order to enable subscriptions.');
         }
 
-        this.client = client;
+        this.credentials = credentials;
+        this.fayeClient = this._getFayeClient();
+    }
+
+    _getFayeAuthorization() {
+        return {
+            incoming: (message, callback) => {
+				console.log('incoming',message);
+				return callback(message)
+			},
+            outgoing: (message, callback) => {
+                if (message.subscription && this.subscriptions[message.subscription]) {
+                    const subscription = this.subscriptions[message.subscription];
+
+                    // eslint-disable-next-line no-param-reassign
+                    message.ext = {
+                        user_id: subscription.userId,
+                        api_key: this.credentials.api_key,
+                        signature: subscription.token,
+                    };
+
+					console.log(message.ext)
+                }
+
+                callback(message);
+            },
+        };
+    }
+
+    _getFayeClient(timeout = 10) {
+        if (!this.fayeClient) {
+            this.fayeClient = new Faye.Client('https://faye-us-east.stream-io-api.com/faye', { timeout });
+
+            const authExtension = this._getFayeAuthorization();
+
+            this.fayeClient.addExtension(authExtension);
+        }
+
+        return this.fayeClient;
     }
 
     publish = () => Promise.resolve();
 
-    subscribe(triggerName, onMessage) {
-        const feedParts = triggerName.split(':');
+    subscribe(feed, onMessage) {
+        const id = `site-${this.credentials.app_id}-feed-${feed}`;
 
-        const id = this.currentSubscriptionId++;
+        const subscription = this._getFayeClient().subscribe(`/${id}`, onMessage);
 
-        const feed = this.client.feed(...feedParts);
+		const token = jwt.sign(
+            {
+                resource: '*',
+                action: '*',
+                feed_id: feed,
+            },
+            this.credentials.api_secret
+        );
 
-        this.subscriptions[id] = feed.subscribe(onMessage);
+        this.subscriptions[`/${id}`] = {
+            token,
+            userId: id,
+            fayeSubscription: subscription,
+        };
 
         return Promise.resolve(id);
     }
 
     unsubscribe(id) {
-        const subscription = this.subscriptions[id];
+        const subscription = this.subscriptions[`/${id}`];
 
-        delete this.subscriptions[id];
+        delete this.subscriptions[`/${id}`];
 
-        subscription.unsubscribe();
+        subscription.fayeSubscription.cancel();
     }
 }
