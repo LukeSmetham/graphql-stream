58011564eac1ac88897ba79922c244d418f904cd
Luke Smetham <luke@arkitek.co.uk>
2021-09-04 19:08:47 +0100

    Update me.js

--- examples/full-example/schema/types/User/resolvers/me.js
+++ examples/full-example/schema/types/User/resolvers/me.js
@@ -3,7 +3,7 @@ export const me = tc => tc.mongooseResolvers
 	.removeArg('_id')
 	.wrapResolve(next => rp => {
 		const { user } = rp.context;
-		console.log(user);
+
 		rp.args._id = user;
 		
 		return next(rp);
