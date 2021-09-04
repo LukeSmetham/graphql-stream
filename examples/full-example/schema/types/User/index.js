b5c767852295103fe489388a614d5ecd987db776
Luke Smetham <luke@arkitek.co.uk>
2021-09-04 19:04:31 +0100

    relate users feed into their user object

--- examples/full-example/schema/types/User/index.js
+++ examples/full-example/schema/types/User/index.js
@@ -1,11 +1,18 @@
 import UserTC from './User';
 import * as resolvers from './resolvers';
-import * as fields from './fields';
 
 Object.keys(resolvers).map(k => UserTC.addResolver(resolvers[k](UserTC)));
 
 UserTC.addFields({
 	token: 'String',
+});
+
+UserTC.addRelation('feed', {
+	prepareArgs: {
+		id: (source) => `user:${source._id.toString()}`
+	},
+	projection: { _id: true },
+	resolver: () => UserTC.schemaComposer.getOTC('StreamUserFeed').getResolver('getFeed')
 })
 
 export default UserTC;
