837783290866326a6e0df10a0bbf39d2afeffb3b
Luke Smetham <luke@arkitek.co.uk>
2021-09-04 17:09:26 +0100

    add mongoose and connect to mongo on boot

--- examples/full-example/main.js
+++ examples/full-example/main.js
@@ -2,17 +2,28 @@ import 'dotenv/config';
 import { createServer } from 'http';
 import { execute, subscribe } from 'graphql';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
-import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import mongoose from 'mongoose';
 
 import schema from './schema';
 
-const { PORT = 8080 } = process.env;
+const { MONGODB_URI, PORT = 8080 } = process.env;
 
+// Connect to MongoDB (top-level await with esm/node 16 removes the need for IIFE/etc.)
+await mongoose.connect(MONGODB_URI, {
+	autoIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Init Express App
 const app = express();
 
+// Init HTTP Server for Subscriptions
 const httpServer = createServer(app);
 
+// Init ApolloServer instance
 const server = new ApolloServer({
     schema,
 	plugins: [{
@@ -26,6 +37,7 @@ const server = new ApolloServer({
 	  }],
 });
 
+// Create a Subscription Server
 const subscriptionServer = SubscriptionServer.create(
     {
         schema,
@@ -38,10 +50,11 @@ const subscriptionServer = SubscriptionServer.create(
     }
 );
 
+// Start the Apollo Server and apply the express app as middleware
 await server.start();
-
 server.applyMiddleware({ app });
 
+// Start!
 httpServer.listen(PORT, () =>
 	console.log(`🚀::${PORT}`)
 );
