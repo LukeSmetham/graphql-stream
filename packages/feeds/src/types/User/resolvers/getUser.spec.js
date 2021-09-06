cb396b508a21e5c03b28b4901041c3226bd4e0b8
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 13:02:06 +0100

    tests the request and potential thrown errors from the resolve method

--- packages/feeds/src/types/User/resolvers/getUser.spec.js
+++ packages/feeds/src/types/User/resolvers/getUser.spec.js
@@ -1,3 +1,4 @@
+import phin from 'phin';
 import { Resolver, schemaComposer } from 'graphql-compose';
 
 import { createUserTC } from '../User';
@@ -7,6 +8,15 @@ const credentials = {
 	api_key: 'STREAM_API_KEY',
 	api_secret: 'STREAM_API_SECRET',
 	app_id: 'STREAM_APP_ID',
+};
+
+const resolveParams = {
+	source: {}, 
+	args: {
+		id: 1,
+	},
+	context: {},
+	info: {},
 };
 
 describe('getUser Resolver', () => {
@@ -21,4 +31,41 @@ describe('getUser Resolver', () => {
 
 		expect(resolver).toBeInstanceOf(Resolver)
 	})
+
+	test('makes a GET request to the /users/:id endpoint', () => {
+		const resolver = getUser(UserTC, { credentials });
+
+		phin.mockImplementationOnce(options => Promise.resolve({ body: options }));
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('GET');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/user/${resolveParams.args.id}?api_key=${credentials.api_key}`);
+		})
+	});
+	
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = getUser(UserTC, { credentials });
+
+		phin.mockImplementationOnce(options => Promise.resolve({ 
+			body: {
+				status_code: 404,
+				detail: 'User does not exist.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/User does not exist./);
+	});
+	
+	test('throws an error if no credentials are passed to the resolver creator function', () => {
+		const resolver = getUser(UserTC);
+
+		phin.mockImplementationOnce(options => Promise.resolve({ 
+			body: {
+				status_code: 404,
+				detail: 'User does not exist.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Missing Stream Credentials/);
+	});
 });
