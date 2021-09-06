7f04f83d831a4153d5a6e7871d133c67dc22ab0e
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 18:45:53 +0100

    test fetching and error handling for the resolve method

--- packages/feeds/src/types/ActivityReaction/resolvers/removeReaction.spec.js
+++ packages/feeds/src/types/ActivityReaction/resolvers/removeReaction.spec.js
@@ -1,3 +1,4 @@
+import phin from 'phin';
 import { Resolver, schemaComposer } from 'graphql-compose';
 
 import { createActivityReactionTC } from '../ActivityReaction';
@@ -7,6 +8,15 @@ const credentials = {
 	api_key: 'STREAM_API_KEY',
 	api_secret: 'STREAM_API_SECRET',
 	app_id: 'STREAM_APP_ID',
+};
+
+const resolveParams = {
+	source: {},
+	args: {
+		id: '1',
+	},
+	context: {},
+	info: {},
 };
 
 describe('removeReaction Resolver', () => {
@@ -20,5 +30,28 @@ describe('removeReaction Resolver', () => {
 		const resolver = removeReaction(ActivityReactionTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
-	})
+	});
+
+	test('makes a DELETE request to the /reaction endpoint', () => {
+		const resolver = removeReaction(ActivityReactionTC, { credentials });
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('DELETE');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/reaction/${resolveParams.args.id}?api_key=${credentials.api_key}`);
+			expect(response.removed).toEqual(resolveParams.args.id);
+		})
+	});
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = removeReaction(ActivityReactionTC, { credentials });
+
+		phin.mockImplementationOnce(() => Promise.resolve({ 
+			body: {
+				status_code: 500,
+				detail: 'Something went wrong.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Something went wrong./);
+	});
 });
