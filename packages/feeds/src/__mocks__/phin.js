ec5316a23cc639ff2809e34aee11805413383247
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 13:01:23 +0100

    simplify the phin mock
    Easier to override in individual tests than cover all cases with conditions in the manual mock

--- packages/feeds/src/__mocks__/phin.js
+++ packages/feeds/src/__mocks__/phin.js
@@ -1,15 +1,3 @@
-export default jest.fn().mockImplementation(options =>
-	new Promise(res => {
-		if (options.data.id === 2) {
-			res({
-				body: {
-					status_code: 404,
-					detail: 'Not found!',
-				}
-			})
-		} else {
-			res({
-				body: options
-			});
-		}
-	}));
+export default jest.fn().mockImplementation(options => Promise.resolve({
+	body: options
+}));
