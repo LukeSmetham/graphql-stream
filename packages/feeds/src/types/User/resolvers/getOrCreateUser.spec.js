977482ff58653489001e403e4133ff774ce96c4f
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 10:50:23 +0100

    test resolve method with mocked phin

--- packages/feeds/src/types/User/resolvers/getOrCreateUser.spec.js
+++ packages/feeds/src/types/User/resolvers/getOrCreateUser.spec.js
@@ -9,6 +9,19 @@ const credentials = {
 	app_id: 'STREAM_APP_ID',
 };
 
+const resolverParams = {
+	source: {}, 
+	args: {
+		id: 1,
+		data: {
+			name: 'Luke',
+			email: 'luke@getstream.io'
+		}
+	},
+	context: {},
+	info: {},
+};
+
 describe('getOrCreateUser Resolver', () => {
 	let UserTC;
 	beforeAll(() => {
@@ -20,5 +33,15 @@ describe('getOrCreateUser Resolver', () => {
 		const resolver = getOrCreateUser(UserTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
+	})
+
+	test('makes a POST request to the /user endpoint with the get_or_create parameter', () => {
+		const resolver = getOrCreateUser(UserTC, { credentials });
+
+		resolver.resolve(resolverParams).then((response) => {
+			expect(response.method).toEqual('POST');
+			expect(response.url).toEqual('https://api.stream-io-api.com/api/v1.0/user?api_key=STREAM_API_KEY&get_or_create=true');
+			expect(response.data).toEqual(resolverParams.args);
+		})
 	})
 });
