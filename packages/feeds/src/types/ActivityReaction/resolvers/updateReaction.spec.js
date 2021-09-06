07fa215ceeeb995d06beec41ccda82f960d2cdab
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 18:51:21 +0100

    added tests for fetch request and error handling for the resolve method

--- packages/feeds/src/types/ActivityReaction/resolvers/updateReaction.spec.js
+++ packages/feeds/src/types/ActivityReaction/resolvers/updateReaction.spec.js
@@ -1,3 +1,4 @@
+import phin from 'phin';
 import { Resolver, schemaComposer } from 'graphql-compose';
 
 import { createActivityReactionTC } from '../ActivityReaction';
@@ -8,6 +9,18 @@ const credentials = {
 	api_secret: 'STREAM_API_SECRET',
 	app_id: 'STREAM_APP_ID',
 };
+
+const resolveParams = {
+	source: {},
+	args: {
+		data: {
+			kind: 'like'
+		},
+		target_feeds: ['user:1', 'timeline: 1']
+	},
+	context: {},
+	info: {},
+}
 
 describe('updateReaction Resolver', () => {
 	let ActivityReactionTC;
@@ -20,5 +33,28 @@ describe('updateReaction Resolver', () => {
 		const resolver = updateReaction(ActivityReactionTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
-	})
+	});
+
+	test('makes a PUT request to the /reaction/:id endpoint', () => {
+		const resolver = updateReaction(ActivityReactionTC, { credentials });
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('PUT');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/reaction/${resolveParams.args.id}?api_key=${credentials.api_key}`);
+			expect(response.data).toEqual(resolveParams.args);
+		})
+	});
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = updateReaction(ActivityReactionTC, { credentials });
+
+		phin.mockImplementationOnce(() => Promise.resolve({ 
+			body: {
+				status_code: 500,
+				detail: 'Something went wrong.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Something went wrong./);
+	});
 });
