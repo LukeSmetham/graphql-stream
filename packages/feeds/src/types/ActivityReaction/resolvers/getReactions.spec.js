0a492e13e70c9c28134523dab7ed61c2fee753fa
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 19:14:59 +0100

    test the GET request URL setup when providing different args to getReactions

--- packages/feeds/src/types/ActivityReaction/resolvers/getReactions.spec.js
+++ packages/feeds/src/types/ActivityReaction/resolvers/getReactions.spec.js
@@ -34,6 +34,59 @@ describe('getReactions Resolver', () => {
 		expect(resolver).toBeInstanceOf(Resolver)
 	});
 
+	test('makes a GET request to the correct endpoint based on the provided args', () => {
+		const resolver = getReactions(ActivityReactionTC, { credentials });
+
+		phin.mockImplementation(options => Promise.resolve({
+			body: {
+				results: options
+			}
+		}))
+
+		const rp1 = {
+			args: {
+				user: 1,
+				options: {
+					id_lte: 1,
+				}
+			}
+		}
+		
+		resolver.resolve(rp1).then((response) => {
+			expect(response.method).toEqual('GET');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/reaction/user_id/${rp1.args.user}?api_key=${credentials.api_key}&id_lte=1`);
+		})
+		
+		const rp2 = {
+			args: {
+				activity: 1,
+				options: {
+					limit: 2,
+					id_lt: 1,
+				}
+			}
+		}
+		
+		resolver.resolve(rp2).then((response) => {
+			expect(response.method).toEqual('GET');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/reaction/activity_id/${rp2.args.activity}?api_key=${credentials.api_key}&limit=2&id_lt=1`);
+		})
+		
+		const rp3 = {
+			args: {
+				parent: 1,
+				options: {
+					id_gt: 1,
+				}
+			}
+		}
+		
+		resolver.resolve(rp3).then((response) => {
+			expect(response.method).toEqual('GET');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/reaction/reaction_id/${rp3.args.parent}?api_key=${credentials.api_key}&id_gt=1`);
+		})
+	});
+
 	test('throws an error if the body contains a status_code property', () => {
 		const resolver = getReactions(ActivityReactionTC, { credentials });
 
