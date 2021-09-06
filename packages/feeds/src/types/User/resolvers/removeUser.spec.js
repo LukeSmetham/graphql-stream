bc5506be2a9c21e0390bfd987348c6b26416751d
Luke Smetham <luke@arkitek.co.uk>
2021-09-06 16:59:22 +0100

    test error cases for the resolve method

--- packages/feeds/src/types/User/resolvers/removeUser.spec.js
+++ packages/feeds/src/types/User/resolvers/removeUser.spec.js
@@ -1,3 +1,4 @@
+;import phin from 'phin'
 import { Resolver, schemaComposer } from 'graphql-compose';
 
 import { createUserTC } from '../User';
@@ -7,6 +8,15 @@ const credentials = {
 	api_key: 'STREAM_API_KEY',
 	api_secret: 'STREAM_API_SECRET',
 	app_id: 'STREAM_APP_ID',
+};
+
+const resolveParams = {
+	source: {}, 
+	args: {
+		id: 1,
+	},
+	context: {},
+	info: {},
 };
 
 describe('removeUser Resolver', () => {
@@ -20,5 +30,27 @@ describe('removeUser Resolver', () => {
 		const resolver = removeUser(UserTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
-	})
+	});
+
+	test('makes a DELETE request to the /users/:id endpoint', () => {
+		const resolver = removeUser(UserTC, { credentials });
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('DELETE');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/user/${resolveParams.args.id}?api_key=${credentials.api_key}`);
+		})
+	});
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = removeUser(UserTC, { credentials });
+
+		phin.mockImplementationOnce(options => Promise.resolve({ 
+			body: {
+				status_code: 404,
+				detail: 'User does not exist.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/User does not exist./);
+	});
 });
