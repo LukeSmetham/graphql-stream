f564421b2092bb14324a0a159db65f92c424a60e
Luke Smetham <luke@arkitek.co.uk>
2021-09-07 10:56:33 +0100

    test coverage for getActivities resolver

---
+++ packages/feeds/src/types/Activity/resolvers/getActivities.spec.js
@@ -0,0 +1,74 @@
+import phin from 'phin';
+import { Resolver, deepmerge, schemaComposer } from 'graphql-compose';
+
+import { StreamID } from 'scalars/StreamID';
+import { createActivityInterfaces } from 'interfaces/Activity';
+import { ensureScalars } from 'composeActivityFeed/ensureScalars';
+
+import { createActivityTC } from '../Activity';
+import { getActivities } from './getActivities';
+
+const credentials = {
+	api_key: 'STREAM_API_KEY',
+	api_secret: 'STREAM_API_SECRET',
+	app_id: 'STREAM_APP_ID',
+};
+
+const resolveParams = {
+	source: {}, 
+	args: {
+		feed: new StreamID('user:1'),
+	},
+	context: {},
+	info: {},
+};
+
+describe('getActivities Resolver', () => {
+	let ActivityTC;
+	beforeAll(() => {
+		schemaComposer.clear();
+		ensureScalars(schemaComposer)
+		createActivityInterfaces(schemaComposer);
+		ActivityTC = createActivityTC({
+			schemaComposer,
+			feed: {
+				feedGroup: 'user',
+				type: 'flat',
+			}
+		});
+	});
+
+	test('returns a graphql-compose resolver instance', () => {
+		const resolver = getActivities(ActivityTC, { credentials });
+
+		expect(resolver).toBeInstanceOf(Resolver)
+	})
+	
+	test('makes a GET request to the /feed/:feed/:id endpoint', () => {
+		const resolver = getActivities(ActivityTC, { credentials });
+
+		phin.mockImplementationOnce((options) => Promise.resolve({ 
+			body: {
+				results: options
+			} 
+		}));
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('GET');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/feed/${resolveParams.args.feed.uri}?api_key=${credentials.api_key}`);
+		})
+	})
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = getActivities(ActivityTC, { credentials });
+
+		phin.mockImplementationOnce(() => Promise.resolve({ 
+			body: {
+				status_code: 500,
+				detail: 'Something went wrong.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Something went wrong./);
+	});
+});
