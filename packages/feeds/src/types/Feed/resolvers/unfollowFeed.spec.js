2e8ac283107448449b7e51c6fe34221d8677cf8f
Luke Smetham <luke@arkitek.co.uk>
2021-09-07 14:04:42 +0100

    test unfollow request and error cases in the resolve method

--- packages/feeds/src/types/Feed/resolvers/unfollowFeed.spec.js
+++ packages/feeds/src/types/Feed/resolvers/unfollowFeed.spec.js
@@ -1,4 +1,7 @@
-import { Resolver, schemaComposer } from 'graphql-compose';
+import phin from 'phin';
+import { Resolver, deepmerge, schemaComposer } from 'graphql-compose';
+
+import { StreamID } from 'scalars/StreamID';
 
 import { createFeedTC } from '../Feed';
 import { unfollowFeed } from './unfollowFeed';
@@ -17,6 +20,16 @@ const options = {
 	}
 };
 
+const resolveParams = {
+	source: {},
+	args: {
+		feed: new StreamID('timeline:1'),
+		target: new StreamID('user:1')
+	},
+	context: {},
+	info: {},
+}
+
 describe('unfollowFeed resolver', () => {
 	let FeedTC;
 	beforeAll(() => {
@@ -28,5 +41,42 @@ describe('unfollowFeed resolver', () => {
 		const resolver = unfollowFeed(FeedTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
+	});
+
+	test('makes a DELETE request to the /feed/:feed/:id/following/target:feed endpoint', () => {
+		const resolver = unfollowFeed(FeedTC, { credentials });
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('DELETE');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/feed/${resolveParams.args.feed.uri}/following/${resolveParams.args.target.toString()}?api_key=${credentials.api_key}`);
+		})
+	})
+	
+	test('appends keep_history parameter to the URL if the keepHistory arg is present', () => {
+		const resolver = unfollowFeed(FeedTC, { credentials });
+
+		const resolveParams2 = deepmerge(resolveParams, {
+			args: {
+				keepHistory: false
+			}
+		});
+
+		resolver.resolve(resolveParams2).then((response) => {
+			expect(response.method).toEqual('DELETE');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/feed/${resolveParams2.args.feed.uri}/following/${resolveParams2.args.target.toString()}?api_key=${credentials.api_key}&keep_history=false`);
+		})
+	});
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = unfollowFeed(FeedTC, { credentials });
+
+		phin.mockImplementationOnce(() => Promise.resolve({ 
+			body: {
+				status_code: 404,
+				detail: 'Feed does not exist.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Feed does not exist./);
 	});
 });
