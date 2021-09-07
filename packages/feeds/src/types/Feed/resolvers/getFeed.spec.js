f3df03023304ee099580d7c93502353544e8b527
Luke Smetham <luke@arkitek.co.uk>
2021-09-07 13:09:50 +0100

    test coverage for getFeed

---
+++ packages/feeds/src/types/Feed/resolvers/getFeed.spec.js
@@ -0,0 +1,47 @@
+import { Resolver, schemaComposer } from 'graphql-compose';
+
+import { createFeedTC } from '../Feed';
+import { getFeed } from './getFeed';
+
+const credentials = {
+	api_key: 'STREAM_API_KEY',
+	api_secret: 'STREAM_API_SECRET',
+	app_id: 'STREAM_APP_ID',
+};
+
+const options = {
+	schemaComposer,
+	feed: {
+		feedGroup: 'user',
+		type: 'flat',
+	}
+};
+
+const resolveParams = {
+	source: {},
+	args: {
+		id: 'user:1'
+	},
+	context: {},
+	info: {},
+}
+
+describe('getFeed resolver', () => {
+	let FeedTC;
+	beforeAll(() => {
+		schemaComposer.clear();
+		FeedTC = createFeedTC(options);
+	});
+
+	test('returns a graphql-compose resolver instance', () => {
+		const resolver = getFeed(FeedTC, { credentials });
+
+		expect(resolver).toBeInstanceOf(Resolver)
+	});
+	
+	test('returns an object with an `id` property equivalent to `args.id`', () => {
+		const resolver = getFeed(FeedTC, { credentials });
+
+		expect(resolver.resolve(resolveParams).id).toBe(resolveParams.args.id)
+	});
+});
