2536d20b8ff75060d2bc3e61b87fe0b487a37423
Luke Smetham <luke@arkitek.co.uk>
2021-09-07 13:50:42 +0100

    test POST request & error cases for the resolve method

--- packages/feeds/src/types/Feed/resolvers/followFeed.spec.js
+++ packages/feeds/src/types/Feed/resolvers/followFeed.spec.js
@@ -1,4 +1,7 @@
-import { Resolver, schemaComposer } from 'graphql-compose';
+import phin from 'phin';
+import { deepmerge, Resolver, schemaComposer } from 'graphql-compose';
+
+import { StreamID } from 'scalars/StreamID';
 
 import { createFeedTC } from '../Feed';
 import { followFeed } from './followFeed';
@@ -17,6 +20,16 @@ const options = {
 	}
 };
 
+const resolveParams = {
+	source: {},
+	args: {
+		feed: new StreamID('timeline:1'),
+		target: new StreamID('user:1')
+	},
+	context: {},
+	info: {},
+}
+
 describe('followFeed resolver', () => {
 	let FeedTC;
 	beforeAll(() => {
@@ -28,5 +41,42 @@ describe('followFeed resolver', () => {
 		const resolver = followFeed(FeedTC, { credentials });
 
 		expect(resolver).toBeInstanceOf(Resolver)
+	});
+
+	test('makes a POST request to the /feed/:feed/:id/follows endpoint', () => {
+		const resolver = followFeed(FeedTC, { credentials });
+
+		resolver.resolve(resolveParams).then((response) => {
+			expect(response.method).toEqual('POST');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/feed/${resolveParams.args.feed.uri}/follows?api_key=${credentials.api_key}`);
+		})
+	})
+	
+	test('appends activity_copy_limit parameter to the URL if present', () => {
+		const resolver = followFeed(FeedTC, { credentials });
+
+		const resolveParams2 = deepmerge(resolveParams, {
+			args: {
+				activityCopyLimit: 10
+			}
+		});
+
+		resolver.resolve(resolveParams2).then((response) => {
+			expect(response.method).toEqual('POST');
+			expect(response.url).toEqual(`https://api.stream-io-api.com/api/v1.0/feed/${resolveParams2.args.feed.uri}/follows?api_key=${credentials.api_key}&activity_copy_limit=10`);
+		})
+	})
+
+	test('throws an error if the body contains a status_code property', () => {
+		const resolver = followFeed(FeedTC, { credentials });
+
+		phin.mockImplementationOnce(() => Promise.resolve({ 
+			body: {
+				status_code: 404,
+				detail: 'Feed does not exist.'
+			} 
+		}));
+
+		expect(() => resolver.resolve(resolveParams)).rejects.toThrow(/Feed does not exist./);
 	});
 });
