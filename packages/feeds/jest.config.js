7dd62f88bec1262e445c06476de75d86b470018c
Luke Smetham <luke@arkitek.co.uk>
2021-09-08 16:47:37 +0100

    added coverageThreshold to jest.config.js

--- packages/feeds/jest.config.js
+++ packages/feeds/jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
 	transform: {
 	  "^.+\\.js?$": ['babel-jest', {rootMode: "upward"}]
 	},
+	"coverageThreshold": {
+		"global": {
+			"branches": 100,
+			"functions": 100,
+			"lines": 100,
+			"statements": 100
+		}
+	}
   }
